1df46e1f623172da1a115c147ba3119ad6d9321d
Volodymyr <vovkul@gmail.com>
2023-11-22 22:38:55 +0100

    task 1 render

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,16 @@
-import user from './user.json';
+import user from '../data/user.json';
 import Profile from 'components/Profile/Profile';
 
 const App = () => (
-  <Profile
-    username={user.username}
-    tag={user.tag}
-    location={user.location}
-    avatar={user.avatar}
-    stats={user.stats}
-  />
+  <div>
+    <Profile
+      username={user.username}
+      tag={user.tag}
+      location={user.location}
+      avatar={user.avatar}
+      stats={user.stats}
+    />
+  </div>
 );
 
 export default App;
