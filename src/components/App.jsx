1af3480d2e89670d6d5514b2ca61ec19f4aca01b
Volodymyr <vovkul@gmail.com>
2023-11-22 17:00:03 +0100

    task 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,14 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import user from './user.json';
+import Profile from 'components/Profile/Profile';
+
+const App = () => (
+  <Profile
+    username={user.username}
+    tag={user.tag}
+    location={user.location}
+    avatar={user.avatar}
+    stats={user.stats}
+  />
+);
+
+export default App;
---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,45 @@
+const Profile = ({
+  avatar,
+  username,
+  tag,
+  location,
+  stats: { followers, views, likes },
+}) => (
+  <div class="profile">
+    <div class="description">
+      <img src={avatar} alt={username} class="avatar" />
+      <p class="name">{username}</p>
+      <p class="tag">{tag}</p>
+      <p class="location">{location}</p>
+    </div>
+
+    <ul class="stats">
+      <li>
+        <span class="label">Followers</span>
+        <span class="quantity">{followers}</span>
+      </li>
+      <li>
+        <span class="label">Views</span>
+        <span class="quantity">{views}</span>
+      </li>
+      <li>
+        <span class="label">Likes</span>
+        <span class="quantity">{likes}</span>
+      </li>
+    </ul>
+  </div>
+);
+
+export default Profile;
+
+// {
+//   "username": "Jacques Gluke",
+//   "tag": "jgluke",
+//   "location": "Ocho Rios, Jamaica",
+//   "avatar": "https://cdn-icons-png.flaticon.com/512/2922/2922506.png",
+//   "stats": {
+//     "followers": 5603,
+//     "views": 4827,
+//     "likes": 1308
+//   }
+// }
