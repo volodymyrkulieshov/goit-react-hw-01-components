3653ec47f7d35d4fd84aa775845051a95ae383b7
Volodymyr <vovkul@gmail.com>
2023-11-27 15:05:47 +0100

    add task 4 with styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,11 @@
 import user from '../data/user.json';
 import data from '../data/data.json';
 import friends from '../data/friends.json';
+import transactions from '../data/transactions.json';
 import Profile from 'components/Profile/Profile';
 import Statistics from 'components/Statistics/Statistics';
 import FriendList from 'components/FriendList/FriendList';
-// import FriendListItem from 'components/FriendList/FriendListItem';
+import TransactionHistory from 'components/Transactions/TransactionHistory';
 
 const App = () => (
   <div>
@@ -18,6 +19,7 @@ const App = () => (
     {/* <Statistics title="Upload stats" stats={data} /> */}
     <Statistics stats={data} />
     <FriendList friends={friends} />
+    <TransactionHistory items={transactions} />;
   </div>
 );
 
